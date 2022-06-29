4eda3f5691ff8664674a5dd3aefc36ee39a5fbd8
NiliChennili <chen_ni_li@hotmail.com>
2022-06-29 10:46:39 +0800

    Assignment 3

---
+++ Assignment 03/Assignment03/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import DemoChoroplethMap from './DemoChoroplethMap'
+import React,{Component} from "react";
+
+class App extends Component {
+  
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <DemoChoroplethMap/>
+    
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
